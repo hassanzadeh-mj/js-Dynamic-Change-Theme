b491d95536cc2027f93c1c0c735590fcb42a965c
Mojtabahassanzadeh <mj.hassanzadeh1995@gmail.com>
2022-07-07 13:11:35 +0430

    add : js codes

--- js/script.js
+++ js/script.js
@@ -4,7 +4,14 @@ const Colors = document.querySelectorAll(".btn")
 
 Colors.forEach(Color=> {
 Color.addEventListener("click", (event) => {
-    let btnColor=event.target.data
-    console.log(btnColor)
+    let btnColor=event.target.dataset.color
+    document.documentElement.style.setProperty( "--theme-color",btnColor )
 })
 })
+// in react js
+// Colors.forEach(Color=> {
+//     Color.addEventListener("click", (event) => {
+//         let btnColor=event.currentTarget.dataset.color
+//         document.documentElement.style.setProperty( "--theme-color",`${btnColor}` )
+//     })
+//     })
